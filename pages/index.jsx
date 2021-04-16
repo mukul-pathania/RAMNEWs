7cce0b2b0c692225ebe7456624d8a7d37553159c
Mukul Pathania <mukulpathania96@gmail.com>
2021-04-16 20:40:10 +0530

    Add a responsive hero section to the landing page
    We might change the taglines and hero image etc later.
    
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Heading } from '@chakra-ui/react';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
@@ -7,7 +7,13 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
-      <Heading>Home Page</Heading>
+      <Hero
+        title="Stories That Count, From People Who Care"
+        subtitle="With RAMNEWS you can get all the latest happenings from around the world."
+        image="/images/hero.svg"
+        ctaText="Create your account now"
+        ctaLink="/"
+      />
     </div>
   );
 }
