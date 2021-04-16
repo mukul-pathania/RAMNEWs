510889a9b00625affa00ca7550d0f0261a17fb33
Mukul Pathania <mukulpathania96@gmail.com>
2021-04-16 14:40:48 +0530

    Setup fonts that might be required for our project
    Added a temporary reference page for fonts.
    
    Signed-off-by: Mukul Pathania <mukulpathania96@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import '../global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
---
+++ pages/fonts.jsx
@@ -0,0 +1,27 @@
+export default function fonts() {
+  return (
+    <div>
+      <p style={{ fontFamily: 'Logo', fontSize: '2rem' }}>RAMNEWS</p>
+      <p style={{ fontFamily: 'Logo', fontSize: '2rem' }}>Logo-Font</p>
+      <p style={{ fontFamily: 'Gudea-Regular', fontSize: '2rem' }}>Gudea-Regular</p>
+      <p style={{ fontFamily: 'Gudea-Italic', fontSize: '2rem' }}>Gudea-Italic</p>
+      <p style={{ fontFamily: 'Gudea-Bold', fontSize: '2rem' }}>Gudea-Bold</p>
+      <p style={{ fontFamily: 'Rajdhani-Light', fontSize: '2rem' }}>Rajdhani-Light</p>
+      <p style={{ fontFamily: 'Rajdhani-Regular', fontSize: '2rem' }}>Rajdhani-Regular</p>
+      <p style={{ fontFamily: 'Rajdhani-Medium', fontSize: '2rem' }}>Rajdhani-Medium</p>
+      <p style={{ fontFamily: 'Rajdhani-SemiBold', fontSize: '2rem' }}>Rajdhani-SemiBold</p>
+      <p style={{ fontFamily: 'Rajdhani-Bold', fontSize: '2rem' }}>Rajdhani-Bold</p>
+      <p style={{ fontFamily: 'LibreFranklin-Thin', fontSize: '2rem' }}>LibreFranklin-Thin</p>
+      <p style={{ fontFamily: 'LibreFranklin-ExtraLight', fontSize: '2rem' }}>
+        LibreFranklin-ExtraLight
+      </p>
+      <p style={{ fontFamily: 'LibreFranklin-Light', fontSize: '2rem' }}>LibreFranklin-Light</p>
+      <p style={{ fontFamily: 'LibreFranklin-Regular', fontSize: '2rem' }}>LibreFranklin-Regular</p>
+      <p style={{ fontFamily: 'LibreFranklin-Medium', fontSize: '2rem' }}>LibreFranklin-Medium</p>
+      <p style={{ fontFamily: 'LibreFranklin-SemiBold', fontSize: '2rem' }}>
+        LibreFranklin-SemiBold
+      </p>
+      <p style={{ fontFamily: 'LibreFranklin-Bold', fontSize: '2rem' }}>LibreFranklin-Bold</p>
+    </div>
+  );
+}
