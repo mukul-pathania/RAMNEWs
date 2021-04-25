ab165a2f7906f45e593aff2f8f41dcfb20ac909d
Rohit <rohitpatil07122001@gmail.com>
2021-04-25 20:35:57 +0530

    Add design for article card
    Signed-off-by: Rohit <rohitpatil07122001@gmail.com>

--- components/Headlines/ArticleCard.jsx
+++ components/Headlines/ArticleCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Link, Text } from '@chakra-ui/react';
+import { Box, Button, Link, Text, Image } from '@chakra-ui/react';
 import moment from 'moment';
 
 /**
@@ -14,12 +14,53 @@ import moment from 'moment';
  */
 
 const ArticleCard = ({ article }) => (
-  <Box p={6} boxShadow="sm" bg="white" w="100%" borderRadius="lg">
-    <Text>{article.title}</Text>
-    <Text>{moment(article.publishedAt).fromNow()}</Text>
-    <Link href={article.url} target="_blank">
-      <Button>Read More</Button>
-    </Link>
+  <Box
+    d="flex"
+    p={6}
+    boxShadow="sm"
+    bg="white"
+    w="100%"
+    borderRadius="lg"
+    flexDirection={{ base: 'column', md: 'column', lg: 'row' }}
+    justifyContent="space-around"
+  >
+    <Box
+      p="4"
+      d="flex"
+      //   border="1px solid black"
+      w={{ base: '100%', md: '90%', lg: '90%' }}
+      flexDirection="column"
+    >
+      <h1
+        style={{
+          fontFamily: 'Rajdhani-SemiBold',
+          fontSize: '1.8rem',
+          textTransform: 'uppercase',
+        }}
+      >
+        {article.title}
+      </h1>
+      <p style={{ fontFamily: 'Rajdhani-SemiBold', fontSize: '1.3rem' }}>
+        {moment(article.publishedAt).fromNow()}
+      </p>
+      <p style={{ fontFamily: 'Rajdhani-SemiBold', fontSize: '1.3rem' }}>
+        Source : {article.source.name}
+      </p>
+      <ul>
+        <li>
+          <p style={{ fontFamily: 'Rajdhani-Medium', fontSize: '1.5rem' }}>{article.description}</p>
+        </li>
+      </ul>
+      <Link href={article.url} target="_blank">
+        <Button bg="#00C896">Read More</Button>
+      </Link>
+    </Box>
+    <Image
+      width={{ base: '50%', md: '50%', lg: '35%' }}
+      height={{ base: '100%', md: '100%', lg: '100%' }}
+      borderRadius="lg"
+      src={article.urlToImage}
+    />
   </Box>
 );
 
