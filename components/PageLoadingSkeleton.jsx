cd806842537566785e39ba373d31fb10ce5a7c7b
Mukul Pathania <mukulpathania96@gmail.com>
2021-06-08 17:03:59 +0530

    Add react-spinners for loading animation

--- components/PageLoadingSkeleton.jsx
+++ components/PageLoadingSkeleton.jsx
@@ -1,8 +1,10 @@
-import { CircularProgress, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import { GridLoader } from 'react-spinners';
 
 const PageLoadingSkeleton = () => (
   <Flex minW="100vw" minH="100vh" align="center" justify="center">
-    <CircularProgress isIndeterminate size="50vh" thickness={4} color="#00c896" />
+    {/* <CircularProgress isIndeterminate size="50vh" thickness={4} color="#00c896" /> */}
+    <GridLoader loading size={60} margin={5} color="#00c896" />
   </Flex>
 );
 
