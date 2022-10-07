dcbe6cf67525d9233f2b626c74eddd2dd9366c11
Mukul Pathania <mukulpathania96@gmail.com>
2022-10-07 20:01:15 +0530

    Add UI for viewing summary of articles

--- components/Headlines/ArticleCard.jsx
+++ components/Headlines/ArticleCard.jsx
@@ -1,20 +1,20 @@
-import { Box, Button, Link, Image } from '@chakra-ui/react';
+import { Box, Button, Link, Image, HStack } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import moment from 'moment';
 
 /**
- *
- * @Rohit please work on this component, this component will receive an article in the props
- * you will have to do the tough work of making it look good. Please try to make it like google news.
- * All the required data is provided in the article, please check what data is present in the article
- * and display it accordingly and also be beware that you may have to format some of the data given and
- * also sometimes some data we are relying upon is not present in the article.
- *
- *
- * Use moment to render the timestamp.
- */
+*
+* @Rohit please work on this component, this component will receive an article in the props
+* you will have to do the tough work of making it look good. Please try to make it like google news.
+* All the required data is provided in the article, please check what data is present in the article
+* and display it accordingly and also be beware that you may have to format some of the data given and
+* also sometimes some data we are relying upon is not present in the article.
+*
+*
+* Use moment to render the timestamp.
+*/
 
-const ArticleCard = ({ article }) => (
+const ArticleCard = ({ article, getSummary }) => (
   <Box
     d="flex"
     p={6}
@@ -52,21 +52,24 @@ const ArticleCard = ({ article }) => (
           <p style={{ fontFamily: 'Rajdhani-Medium', fontSize: '1.5rem' }}>{article.description}</p>
         </li>
       </ul>
-      <Link
-        as={NextLink}
-        href={article.url}
-        target="_blank"
-        style={{ maxWidth: '10rem', marginTop: '2rem' }}
-      >
-        <Button bg="#00C896">Read More</Button>
-      </Link>
+      <HStack spacing="24px">
+        <Link
+          as={NextLink}
+          href={article.url}
+          target="_blank"
+          style={{ maxWidth: '10rem', marginTop: '2rem' }}
+        >
+          <Button bg="#00C896" width="full">Read More</Button>
+        </Link>
+        <Button bg="#000" color="#fff" width="full" _hover={{bg: "#ccc", color: "#000"}} onClick={()=>getSummary(article.url, article.urlToImage)}>View Summary</Button>
+      </HStack>
     </Box>
     <Image
       width={{ base: '50%', md: '50%', lg: '35%' }}
       height={{ base: '100%', md: '100%', lg: '100%' }}
       borderRadius="lg"
       src={article.urlToImage}
-    />
+      />
   </Box>
 );
 
--- components/Headlines/ArticleList.jsx
+++ components/Headlines/ArticleList.jsx
@@ -1,4 +1,6 @@
-import { Alert, AlertIcon, AlertTitle, AlertDescription, VStack } from '@chakra-ui/react';
+import { Alert, AlertIcon, AlertTitle, AlertDescription, VStack, useDisclosure,
+  ModalOverlay, ModalContent, Modal, ModalHeader, ModalCloseButton, Image,
+  ModalBody } from '@chakra-ui/react';
 import { CloseIcon } from '@chakra-ui/icons';
 import { useEffect, useState } from 'react';
 import ArticleSkeleton from './ArticleSkeleton';
@@ -9,6 +11,8 @@ const ArticleList = ({ title, endpoint }) => {
   const test = [1, 2, 3, 4, 5];
   const [articles, setArticles] = useState([]);
   const [error, setError] = useState('');
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  const [summary, setSummary] = useState({sentiment: "", summary: "", title: "", imageUrl: ""});
 
   const getArticles = async () => {
     try {
@@ -23,24 +27,57 @@ const ArticleList = ({ title, endpoint }) => {
     }
   };
 
+  const getSummary = async(articleUrl,imageUrl)=>{
+    try{
+      const resp = await api.post("summarize", {url: articleUrl});
+      if(resp.data.error){
+        setError(resp.data.message); return;
+      }
+      setSummary({summary: resp.data.summary, title: resp.data.title, sentiment: resp.data.sentiment, imageUrl});
+      onOpen();
+
+    }catch(e){
+      setError('An error occured while processing your request');
+    }
+  }
+
   useEffect(() => {
     getArticles();
   }, []);
 
   return (
-    <VStack w={{ base: '100%', md: '80%' }} mx="auto" spacing={4}>
-      {error && (
-        <Alert status="error">
-          <AlertIcon />
-          <AlertTitle mr={2}>Could not process your request</AlertTitle>
-          <AlertDescription>{error}</AlertDescription>
-          <CloseIcon position="absolute" right="8px" top="8px" onClick={() => setError('')} />
-        </Alert>
-      )}
-      {!articles.length && test.map((item) => <ArticleSkeleton key={item} />)}
-      {articles.length &&
-        articles.map((article) => <ArticleCard key={article.title} article={article} />)}
-    </VStack>
+    <>
+      <VStack w={{ base: '100%', md: '80%' }} mx="auto" spacing={4}>
+        {error && (
+          <Alert status="error">
+            <AlertIcon />
+            <AlertTitle mr={2}>Could not process your request</AlertTitle>
+            <AlertDescription>{error}</AlertDescription>
+            <CloseIcon position="absolute" right="8px" top="8px" onClick={() => setError('')} />
+          </Alert>
+        )}
+        {!articles.length && test.map((item) => <ArticleSkeleton key={item} />)}
+        {articles.length &&
+          articles.map((article) => <ArticleCard key={article.title} article={article} getSummary={getSummary} />)}
+      </VStack>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay/>
+        <ModalContent paddingY="35px" paddingX="20px">
+          <ModalHeader>{summary.title}</ModalHeader>
+          <ModalCloseButton/>
+          <ModalBody>
+            <Image
+              borderRadius="lg"
+              marginTop="10px"
+              marginBottom="25px"
+              src={summary.imageUrl}
+              />
+
+            {summary.summary.split('\n').map(str=>(<p>{str}</p>))}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      </>
   );
 };
 
